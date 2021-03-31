9f59be3b3581abdaf4317b4276b2440d005e19ac
Amir Mahdi <amirmahdi.hakari@outlook.com>
2021-04-01 01:23:46 +0430

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function Havane() {
+  var myMusic = document.getElementById("Havane");
+  if (myMusic.paused) {
+    myMusic.play();
+  } else {
+    myMusic.pause();
+  }
+}
+function King() {
+  var myMusic = document.getElementById("king");
+  if (myMusic.paused) {
+    myMusic.play();
+  } else {
+    myMusic.pause();
+  }
+}
+function Closer() {
+  var myMusic = document.getElementById("Closer");
+  if (myMusic.paused) {
+    myMusic.play();
+  } else {
+    myMusic.pause();
+  }
+}
+function Lean() {
+  var myMusic = document.getElementById("Lean");
+  if (myMusic.paused) {
+    myMusic.play();
+  } else {
+    myMusic.pause();
+  }
+}
